3b05a9b0b2195ada74f8013dac982a78ceb3a3ae
Hopson97 <mhopson@hotmail.co.uk>
2018-06-30 11:17:26 +0100

    begin chapter 5

--- Chapter_4/Exercise.hs
+++ Chapter_4/Exercise.hs
@@ -4,7 +4,6 @@ awesome = ["Papuchon", "curry", ":)"]
 also = ["Quake", "The Simons"]
 allAwesome = [awesome, also]
 
-
 --Eq a => [a]
 --This means "A list of type a, where a implements the Eq class"
 --This is needed, because for the `list1 == list2` to work, equality must be checked on each element of the list
---
+++ Chapter_5/Parametricity.hs
@@ -0,0 +1,8 @@
+module Parametricity where 
+
+
+--It is impossible to not return same value
+notSameReturn :: a -> a
+notSameReturn x = x
+notSameReturn x = x * 2
+
