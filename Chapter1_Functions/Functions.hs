3f5014eafc252396fec9fd60738d6da217d6b1b7
Hopson97 <mhopson@hotmail.co.uk>
2018-08-15 01:04:31 +0100

    Complete chapter 2

---
+++ Chapter1_Functions/Functions.hs
@@ -0,0 +1,8 @@
+module Functions where
+
+--Functions name, Args, =, body
+triple x = x * 3
+
+printInc n = print plusTwo
+    where plusTwo = n + 2
+
---
+++ Chapter1_Functions/Learn.hs
@@ -0,0 +1,4 @@
+module Learn where
+
+x = 10 * 5 + y
+y = 10
