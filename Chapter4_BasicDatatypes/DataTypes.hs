596278c9ba405909cdab6a8ff814581ce79588ae
Hopson97 <mhopson@hotmail.co.uk>
2018-08-26 11:30:15 +0100

    Finish chapter 4

--- Chapter4_BasicDatatypes/DataTypes.hs
+++ Chapter4_BasicDatatypes/DataTypes.hs
@@ -1,6 +1,10 @@
 module DataTypes where 
 
-data Mood = Blah | Woot deriving Show
+data Mood = Blah | Woot
+
+instance Show Mood where 
+    show Blah = "blahhhh"
+    show woot = "WOOT!!"
 
 -- Name: Mood 
 -- Values: Blah, or Woot 
@@ -10,3 +14,19 @@ changeMood :: Mood -> Mood
 changeMood Blah = Woot
 changeMood Woot = Blah
 
+--if
+greetIfCool :: String -> IO () 
+greetIfCool coolness = 
+    if isCool coolness
+        then putStrLn "Ayy, your cool bruh"
+    else 
+        putStrLn "Pffft" 
+    where isCool x =
+            x == "I'm cool!"
+
+--Tuples
+printTupleFirst :: Show a => (a, b) -> IO ()
+printTupleFirst tup = print $ fst tup
+
+printTupleSecond :: Show b => (a, b) -> IO ()
+printTupleSecond tup = print $ snd tup
---
+++ Chapter4_BasicDatatypes/Exercises.hs
@@ -0,0 +1,13 @@
+module Exercises where 
+
+isPalindrone :: (Eq a) => [a] -> Bool 
+isPalindrone x = x == reverse x
+
+myAbs :: Integer -> Integer
+myAbs n =
+    if n > 0
+        then n
+    else -n
+
+f :: (a, b) -> (c, d) -> ((b, d), (a, c))
+f x y= ((snd x, snd y), (fst x, fst y))
